8e2084d5f1e908fc1f569add488befa7670272a4
AFKMDaniel <danich734@gmail.com>
2022-10-30 20:15:43 +1000

    added drag function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const leftButton = document.querySelector('#left');
 const rightButton = document.querySelector('#right');
 let nextSlide = 1;
 let previousSlide = sliderItems.length-1;
+let canSlide = true;
 
 sliderItems.forEach((el,i) => {
     if(i > 1) el.remove();
@@ -21,12 +22,14 @@ const moveSliderRight = () => {
     rightButton.setAttribute('disabled','true');
     sliderContent.style.transitionDuration = '0.3s';
     sliderContent.style.transform = 'translateX(calc(-100%/3*2))';
+    canSlide = false;
     setTimeout(() => {
         sliderContent.style.transitionDuration = '0s';
         sliderContent.style.transform = 'translateX(calc(-100%/3))';
         sliderContent.appendChild(sliderItems[nextSlide].cloneNode(true));
         renderedSlide.remove();
         rightButton.removeAttribute('disabled');
+        canSlide = true;
     },300)
 }
 
@@ -39,15 +42,52 @@ const moveSliderLeft = () => {
     leftButton.setAttribute('disabled','true');
     sliderContent.style.transitionDuration = '0.3s';
     sliderContent.style.transform = 'translateX(0)';
+    canSlide = false;
     setTimeout(() => {
         sliderContent.style.transitionDuration = '0s';
         sliderContent.style.transform = 'translateX(calc(-100%/3))';
         sliderContent.insertBefore(sliderItems[previousSlide].cloneNode(true),sliderContent.firstChild);
         renderedSlides[renderedSlides.length-1].remove();
         leftButton.removeAttribute('disabled');
+        canSlide = true;
     },300)
 }
 
 rightButton.addEventListener('click', moveSliderRight);
 
 leftButton.addEventListener('click', moveSliderLeft);
+
+const slider = document.querySelector('.slider');
+
+let startX;
+let x;
+let isDown = false;
+
+const mouseDownHandle = (e) => {
+    isDown = true;
+    startX = e.clientX;
+    x = e.clientX;
+}
+
+const mouseMoveHandle = (e) => {
+    if (!isDown) return;
+    x = e.clientX;
+    sliderContent.style.transform = `translateX(calc(-100%/3 - ${(startX-x)/2}px))`;
+    if (Math.abs(startX - x) > slider.offsetWidth/2) mouseUpHandle() ;
+}
+
+const mouseUpHandle = () => {
+    if (!isDown) return;
+    isDown = false;
+    if (startX - x > 0 && canSlide) moveSliderRight();
+    else if(startX - x < 0 && canSlide) moveSliderLeft();
+}
+
+slider.addEventListener('mousedown', mouseDownHandle);
+slider.addEventListener('mousemove', mouseMoveHandle);
+slider.addEventListener('mouseup', mouseUpHandle);
+
+slider.addEventListener('touchstart', e => mouseDownHandle(e.changedTouches[0]));
+slider.addEventListener('touchmove', e => mouseMoveHandle(e.changedTouches[0]));
+slider.addEventListener('touchend', e => mouseUpHandle(e.changedTouches[0]));
+
